6ace21e5ffa0cf3a9b58b048a44994661373c454
Jarret Oldham <joldham@dealertradenetwork.com>
2024-03-21 10:28:31 -0400

    adding trailing slash to fix static export issue

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const withMarkdoc = require("@markdoc/next.js");
 module.exports = withMarkdoc({ schemaPath: "./markdoc", mode: "static" })({
   pageExtensions: ["markdoc", "md", "mdoc", "js", "jsx", "ts", "tsx"],
   output: 'export', 
+  /** 
+   * prevents issue with static exporting where urls wouldn't resolve on refresh. 
+   * easier than setting up an nginx proxy for now
+   * https://nextjs.org/docs/app/building-your-application/deploying/static-exports#deploying
+   * https://nextjs.org/docs/app/api-reference/next-config-js/trailingSlash
+   */
+  trailingSlash: true,
 });
